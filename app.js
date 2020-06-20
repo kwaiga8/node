071903f103f32f65bd4591b5ecb4fd9001344724
argentum <kwaiga8@gmail.com>
2020-06-20 19:42:21 -0400

    update t o check behaviour of AWS pipeline

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res) =>{
-    res.send('Wecome to the home page darling');
+    res.send('Update this simple page');
 
 });
 
