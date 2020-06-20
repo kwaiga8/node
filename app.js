aa3209b1712f2649e855287e387aa3542ad49796
argentum <kwaiga8@gmail.com>
2020-06-20 17:44:03 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req,res) =>{
+    res.send('Wecome to the home page darling');
+
+});
+
+app.listen(PORT, () => {
+    console.log(`App server is running on port: ${PORT}`);
+});
